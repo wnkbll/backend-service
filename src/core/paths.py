33df70d2ebf53b7c61f89739f1289598f92c5794
Grigoriy Arzhevitin <arzhevitin.g@gmail.com>
2025-03-01 19:26:26 +0500

    add new paths

--- src/core/paths.py
+++ src/core/paths.py
@@ -5,3 +5,6 @@ SRC_DIR = ROOT_DIR.joinpath("src/")
 
 ENV_PATH = ROOT_DIR.joinpath(".env")
 LOGGING_DIR = ROOT_DIR.joinpath("logs/")
+
+STATIC_PATH = ROOT_DIR.joinpath("static/")
+TEMPLATES_PATH = ROOT_DIR.joinpath("templates/")
